0199cf802dd0bd5e1706e994074e89f063e174e1
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2019-11-19 01:34:53 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# StoneMoSScreen
+
+A Screen resolution library that draws objects responsive. Write your game in one resolution and play it in infinite resolutions.
+
+You can use it simply, like this:
+
+```lua
+StoneMoSScreen = require "libs.StoneMoSScreen"; StoneMoSScreen.new(true, 800, 600)
+```
+
+## Functions
+
+### getScaleDimension()
+
+Function that returns scale calculator
+
+### new(override, width, height)
+
+Function that create a instance of the library.
+* override - true if want to override love.graphics.draw default library
+* width - the width your game is designed for
+* height - the height your game is designed for
+
+### create(objectType, drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+
+Works like love.graphics.draw, but it creates a calculated scale and saves it
+
+### draw(drawableObject)
+
+draw the object with the key saved in create function below
+
+### overrideDefaultDraw()
+
+If you want to override the default function sometime you can
+
+### restoreDefaultDraw()
+
+If you have overrided the default draw function, can restore it
---
+++ init.lua
@@ -0,0 +1,86 @@
+local currentPath   = (...):gsub('%.init$', '') .. "."
+local ScaleDimension = require(string.format("%sScaleDimension", currentPath))
+local StoneMoSScreen = {}; StoneMoSScreen.__index = StoneMoSScreen
+local instance = nil; local defaultDraw = love.graphics.draw
+
+local function getScaleDimension()
+    return instance.scaleDimension
+end
+
+function StoneMoSScreen:new()
+    return setmetatable({
+        static = {}, dynamic = {},
+        scaleDimension = ScaleDimension:new()
+    }, StoneMoSScreen)
+end
+
+function StoneMoSScreen:isStatic(drawableObject)
+    return not self.dynamic[drawableObject]
+end
+
+function StoneMoSScreen:create(objectType, drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+    local scales = self.scaleDimension:calculeScales(drawable, ox, oy, x, y)
+    local newScale = {x = scales.scaleX * sx, y = scales.scaleY * sy, originalSize = nil}
+    scales.relative = newScale
+    self[objectType][drawable] = {
+        x = scales.x, y = scales.y, sx = newScale.x, sy = newScale.y,
+        ox = scales.width, oy = scales.height, r = r
+    }
+    return self[objectType][drawable]
+end
+
+function StoneMoSScreen:calculate(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+    local createdNow = false
+    local exists = self.static[drawable] or self.dynamic[drawable] or false
+    if not exists then exists = self:create("dynamic", drawable, x, y, r, sx, sy, ox, oy); createdNow = true end
+    if not instance:isStatic(drawable) and not createdNow then
+        return self:create("dynamic", drawable, x, y, r, sx, sy, ox, oy)
+    end
+    return exists
+end
+
+function StoneMoSScreen:get(drawableObject)
+    return self.static[drawableObject]
+end
+
+local function overrideDraw(...)
+    local parameters = {...}; local drawType = 0
+    if (type(parameters[2]) == "userdata" and type(parameters[3]) == "number") then drawType = 1
+    elseif type(parameters[2]) ~= "number" then defaultDraw(...)
+    else
+        local texture, drawable, x, y, r, sx, sy, ox, oy, kx, ky = parameters[0 + drawType], parameters[1 + drawType], parameters[2 + drawType], parameters[3 + drawType], parameters[4 + drawType], parameters[5 + drawType], parameters[6 + drawType], parameters[7 + drawType], parameters[8 + drawType], parameters[9 + drawType], parameters[10 + drawType]
+        local scales = instance:calculate(drawable, x or 0, y or 0, r or 0, sx or 1, sy or 1, ox or 0, oy or 0)
+        defaultDraw(drawable, scales.x, scales.y, scales.r, scales.sx, scales.sy, scales.ox, scales.oy)
+    end
+end
+
+local function create(objectType, drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+    return instance:create(objectType, drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+end
+
+--[[
+    override - default LÖVE graphics draw
+    width - pretended screen width
+    height - pretended screen height
+]]
+local function new(override, width, height)
+    if not instance then instance = StoneMoSScreen:new() end
+    if override then love.graphics.draw = overrideDraw; local temp = love.resize
+        love.resize = function(w, h) temp(w,h); instance.scaleDimension:screenResize(w, h) end
+    end
+    instance.scaleDimension:setGameScreenScale(width, height)
+end
+
+local function draw(drawableObject)
+    local scales = instance:get(drawableObject)
+    if scales then defaultDraw(drawable, scales.x, scales.y, scales.r, scales.sx, scales.sy, scales.ox, scales.oy) end
+end
+
+local function restoreDefaultDraw() love.graphics.draw = defaultDraw end
+
+local function overrideDefaultDraw() love.graphics.draw = overrideDraw end
+
+return {
+    getScaleDimension = getScaleDimension, restoreDefaultDraw = restoreDefaultDraw,
+    new = new, draw = draw, create = create, overrideDefaultDraw = overrideDefaultDraw
+}
